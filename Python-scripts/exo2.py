e3c9047527e13602ed6e001716d93da267bc80db
Juan Manuel Garcia <jumagarilla@hotmail.com>
2019-05-28 16:43:27 +0200

    Comments on exo2.py

--- Python-scripts/exo2.py
+++ Python-scripts/exo2.py
@@ -19,13 +19,13 @@ for x in f:
     line=x.strip('\n')
     line=line.split('\t')
     if (line[2]==args.match) : 
-        last=line[len(line)-1].split(';')
+        last=line[len(line)-1].split(';') # A new list containing every feature from the last column separated by a semicolon is calculated
         search="Target="
-        res=[i for i in last if search in i]
-        new_res=res[0].replace("Target=","")
+        res=[i for i in last if search in i] # Information about 'Target' is extracted from the previously obtained list
+        new_res=res[0].replace("Target=","") # Substring 'Target=' is removed
         for j in range(0,len(line)-1) :
             new_f.write(line[j]+"\t")
-        new_f.write(new_res+"\n")
+        new_f.write(new_res+"\n") # In the last column, information about 'Target' is just added
 
 f.close()
 new_f.close()
